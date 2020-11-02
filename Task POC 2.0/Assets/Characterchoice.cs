fac873924ea201c6fb710d316db45d97384da5da
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2020-11-02 14:22:46 -0600

    Commit 3
    Starting to figure stuff out

--- Task POC 2.0/Assets/Characterchoice.cs
+++ Task POC 2.0/Assets/Characterchoice.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Characterchoice : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
     
+    
+
 
     void Start()
     {
-        
+        Random.Range(0, 1);
 
-        
-        
+        bool x = spriteRenderer;
 
+        if (x == false)
+        {
+            Color newColor = new Color(255, 0, 0, 0);
+
+        }
+        else if (x == true)
+        {
+            Color newColor = new Color(0, 0, 0, 0);
+
+        }
 
 
     }
@@ -22,7 +35,5 @@ public class Characterchoice : MonoBehaviour
     {
         
         
-
-
     }
 }
