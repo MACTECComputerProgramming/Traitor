81310e649ab39204aad45e16ea12d88d4af2cce2
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2020-11-06 13:37:42 -0600

    final commit

---
+++ Tag System/Assets/Inivisiblity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inivisiblity : MonoBehaviour
+{
+    private Renderer rend;
+
+    [SerializeField]
+    private Color newColor = Color.clear;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rend.material.color = newColor;
+    }
+
+}
---
+++ Tag System/Assets/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private const float SPEED = 50f;
+    public Sprite testsprite2;
+
+    
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveX = +1f;
+        }
+
+        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+
+        transform.position += moveDir * SPEED * Time.deltaTime;
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.GetComponent<SpriteRenderer>().sprite = testsprite2;
+
+        
+    }
+
+
+
+}
